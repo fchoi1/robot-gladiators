bc562477bc92292674e6ba9db5e2c95274a2382f
fchoi1 <masterfabster2010@hotmail.com>
2021-10-24 19:06:54 -0400

    updated game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("This is an alert!! JavaScript is running!");
+// First sample function window.alert("This is an alert!! JavaScript is running!");
+
+// Store Robot name in variable
+var playerName = window.prompt("What is your robot's name?");
+
+// This does math!
+console.log(10 + 10);
+
+// Displays strings
+console.log("This logs a string, good for leaving yourself a message");
+
+// What is this?
+console.log("Our robot's name is: " + playerName);
+
+
+// New Fight Function
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+fight();
